01b2376e364f188aab0d062e3a40dbe64d446f75
CAD97 <cad97@cad97.com>
2015-02-14 21:26:37 -0500

    Setup Episode 3
    (Yes, I'm following along on Let's Mod Reboot again, its been a while since I set up a project OK?)

---
+++ src/main/java/cad97/spawnercraft2/SpawnerCraft2.java
@@ -0,0 +1,31 @@
+package cad97.spawnercraft2;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid="spawnercraft2", name="SpawnerCraft2", version="1.8-2.0")
+public class SpawnerCraft2
+{
+	@Mod.Instance
+	public static SpawnerCraft2 instance;
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		//stub
+	}
+
+	@Mod.EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		//stub
+	}
+
+	@Mod.EventHandler
+	public void postInit(FMLPostInitializationEvent event)
+	{
+
+	}
+}
