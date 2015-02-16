fda1bbb435767c14430a0bc215e395ae0f24a1b0
CAD97 <cad97@cad97.com>
2015-02-16 01:31:30 -0500

    Removed 2
    I don't like it, as this is still the same mod, just version 2.0 recoded if that makes any sense.
    Not SpawnerCraft2, Spawnercraft v2.
    SpawnerCraft2 makes me think reworking.
    
    Anyway, this _should_ give it back compatibility with v1 once I've added in the MobCage block. Don't quote me on that though. It's a complete re-write.

--- src/main/java/cad97/spawnercraft2/reference/Reference.java
+++ src/main/java/cad97/spawnercraft2/reference/Reference.java
@@ -5,8 +5,8 @@ import cad97.spawnercraft2.handler.ConfigHandler;
 
 public class Reference
 {
-	public static final String MOD_ID = "spawnercraft2";
-	public static final String MOD_NAME = "SpawnerCraft2";
+	public static final String MOD_ID = "spawnercraft";
+	public static final String MOD_NAME = "SpawnerCraft";
 	public static final String VERSION = "1.8-2.0";
 
 	public static final String GUI_FACTORY_CLASS = "cad97.spawnercraft2.client.gui.GuiFactory";
