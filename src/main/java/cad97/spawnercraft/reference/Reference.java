3bf322c8590e58d26b09845476f6e7d57c91ed5d
CAD97 <cad97@cad97.com>
2015-07-22 16:07:18 -0400

    Display NBT in a pretty manner.
    No really, that's a 100% objective term.

--- src/main/java/cad97/spawnercraft/reference/Reference.java
+++ src/main/java/cad97/spawnercraft/reference/Reference.java
@@ -17,19 +17,18 @@ public class Reference
 	// Reverse coloration of regular skeletons
 	public static final EntityList.EntityEggInfo witherSkeletonEggInfo = new EntityList.EntityEggInfo("WitherSkeleton", 4802889, 12698049);
 
-	//{EntityId:Skeleton,SpawnData:{SkeletonType:1,Equipment:[{Count:1,id:272},{},{},{},{}]}}
 	public static final NBTTagCompound witherSkeletonSpawnerNBT = new NBTTagCompound();
-	static {
-		witherSkeletonSpawnerNBT.setString("EntityId", "Skeleton");
-		NBTTagCompound spawnData = new NBTTagCompound();
-		spawnData.setByte("SkeletonType", (byte) 1);
-		NBTTagList equipment = new NBTTagList();
-		NBTTagCompound item = new NBTTagCompound();
-		item.setByte("Count",(byte)1);
-		item.setString("id","minecraft:stone_sword");
-		equipment.appendTag(item);
-		spawnData.setTag("Equipment", equipment);
-		witherSkeletonSpawnerNBT.setTag("SpawnData", spawnData);
-		System.out.println(witherSkeletonSpawnerNBT.toString());
+	static {                                                        // {
+		witherSkeletonSpawnerNBT.setString("EntityId", "Skeleton"); //   EntityId:Skeleton,
+		NBTTagCompound spawnData = new NBTTagCompound();            //   SpawnData:{
+		spawnData.setByte("SkeletonType", (byte) 1);                //     SkeletonType:1,
+		NBTTagList equipment = new NBTTagList();                    //     Equipment:[
+		NBTTagCompound item = new NBTTagCompound();                 //       {
+		item.setByte("Count",(byte)1);                              //         Count:1,
+		item.setString("id","minecraft:stone_sword");               //         id:minecraft:stone_sword
+		equipment.appendTag(item);                                  //       }
+		spawnData.setTag("Equipment", equipment);                   //     ]
+		witherSkeletonSpawnerNBT.setTag("SpawnData", spawnData);    //   }
+		System.out.println(witherSkeletonSpawnerNBT.toString());    // }
 	}
 }
